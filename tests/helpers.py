8c6ece2663cf69bac500e73197cca86e3289bb35
ItsDrike <itsdrike@protonmail.com>
2022-05-17 23:39:09 +0200

    Add further info about const_coro to docstring

--- tests/helpers.py
+++ tests/helpers.py
@@ -18,6 +18,9 @@ def const_coro(value: T) -> Callable[..., Awaitable[T]]:
     This is needed because in python 3.7, Mock.return_value didn't properly cover
     async functions, which means we need to do Mock.side_effect = some_coro. This
     function just makes it easy to quickly construct these coroutines.
+
+    This function can be removed once support for 3.7 is dropped, in favor of 3.8+
+    unittest.mock.AsyncMock class, which supports __await__ directly.
     """
 
     async def inner(*a, **kw) -> T:
