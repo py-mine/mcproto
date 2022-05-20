ef8026a55aedff6415f97be8bc6a02db0c92085b
ItsDrike <itsdrike@protonmail.com>
2022-05-20 13:23:17 +0200

    Split protocol helper classes/funcs to separate file

---
+++ tests/protocol/helpers.py
@@ -0,0 +1,178 @@
+from typing import Optional
+from unittest.mock import AsyncMock, Mock
+
+from mcproto.protocol.abc import BaseAsyncReader, BaseAsyncWriter, BaseSyncReader, BaseSyncWriter
+from tests.helpers import SynchronizedMixin
+
+
+class WriteFunctionMock(Mock):
+    def __init__(self, *a, **kw):
+        super().__init__(*a, **kw)
+        self.combined_data = bytearray()
+
+    def __call__(self, data: bytearray) -> None:
+        """Override mock's __call__ to extend our combined_data bytearray.
+
+        This allows us to keep track of exactly what data was written by the mocked write function
+        in total, rather than only having tools like assert_called_with, which don't combine the
+        data of each call.
+        """
+        self.combined_data.extend(data)
+        return super().__call__(data)
+
+    def assert_has_data(self, data: bytearray, ensure_called: bool = True):
+        """Ensure that the total data to write by the mocked function matches expected data."""
+        if ensure_called:
+            self.assert_called()
+
+        if self.combined_data != data:
+            raise AssertionError(f"Write function mock expected data {data!r}, but was {self.call_data!r}")
+
+
+class WriteFunctionAsyncMock(WriteFunctionMock, AsyncMock):
+    ...
+
+
+class ReadFunctionMock(Mock):
+    def __init__(self, *a, combined_data: Optional[bytearray] = None, **kw):
+        super().__init__(*a, **kw)
+        if combined_data is None:
+            combined_data = bytearray()
+        self.combined_data = combined_data
+
+    def __call__(self, length: int) -> bytearray:
+        """Override mock's __call__ to make it return part of our combined_data bytearray.
+
+        This allows us to define the combined data we want the mocked read function to be
+        returning, and have each call only take requested part (length) of that data.
+        """
+        self.return_value = self.combined_data[:length]
+        del self.combined_data[:length]
+        return super().__call__(length)
+
+    def assert_read_everything(self):
+        """Ensure that the passed combined_data was fully read and depleted by one, or more calls."""
+        if len(self.combined_data) != 0:
+            raise AssertionError(
+                f"Read function didn't deplete all of it's data, remaining data: {self.combined_data!r}"
+            )
+
+
+class ReadFunctionAsyncMock(ReadFunctionMock, AsyncMock):
+    ...
+
+
+class SyncWriter(BaseSyncWriter):
+    """Initializable concrete implementation of BaseSyncWriter ABC."""
+
+    def write(self, data: bytearray) -> None:
+        """Concrete implementation of abstract write method.
+
+        Since classes using abc.ABC can't be initialized if they have any abstract methods
+        which weren't overridden with a concrete implementation, this is a fake implementation,
+        without any actual logic, purely to allow the initialization of this class.
+
+        This method is expected to be mocked using WriteFunctionMock if it's expected to get called
+        during testing. If this method gets called without being mocked, it will raise NotImplementedError.
+        """
+        raise NotImplementedError(
+            "This concrete override of abstract write method isn't intended for actual use!\n"
+            " - If you're writing a new test, did you forget to mock it?\n"
+            " - If you're seeing this in an existing test, this method got called without the test expecting it,"
+            " this probably means you changed something in the code leading to this call, but you haven't updated"
+            " the tests to mock this function."
+        )
+
+
+class SyncReader(BaseSyncReader):
+    """Testable concrete implementation of BaseSyncReader ABC."""
+
+    def read(self, length: int) -> bytearray:
+        """Concrete implementation of abstract read method.
+
+        Since classes using abc.ABC can't be initialized if they have any abstract methods
+        which weren't overridden with a concrete implementation, this is a fake implementation,
+        without any actual logic, purely to allow the initialization of this class.
+
+        This method is expected to be mocked using ReadFunctionMock if it's expected to get called
+        during testing. If this method gets called without being mocked, it will raise NotImplementedError.
+        """
+        raise NotImplementedError(
+            "This concrete override of abstract read method isn't intended for actual use!\n"
+            " - If you're writing a new test, did you forget to mock it?\n"
+            " - If you're seeing this in an existing test, this method got called without the test expecting it,"
+            " this probably means you changed something in the code leading to this call, but you haven't updated"
+            " the tests to mock this function."
+        )
+
+
+class AsyncWriter(BaseAsyncWriter):
+    """Initializable concrete implementation of BaseAsyncWriter ABC."""
+
+    async def write(self, data: bytearray) -> None:
+        """Concrete implementation of abstract write method.
+
+        Since classes using abc.ABC can't be initialized if they have any abstract methods
+        which weren't overridden with a concrete implementation, this is a fake implementation,
+        without any actual logic, purely to allow the initialization of this class.
+
+        This method is expected to be mocked using WriteFunctionAsyncMock if it's expected to get called
+        during testing. If this method gets called without being mocked, it will raise NotImplementedError.
+        """
+        raise NotImplementedError(
+            "This concrete override of abstract write method isn't intended for actual use!\n"
+            " - If you're writing a new test, did you forget to mock it?\n"
+            " - If you're seeing this in an existing test, this method got called without the test expecting it,"
+            " this probably means you changed something in the code leading to this call, but you haven't updated"
+            " the tests to mock this function."
+        )
+
+
+class AsyncReader(BaseAsyncReader):
+    """Testable concrete implementation of BaseAsyncReader ABC."""
+
+    async def read(self, length: int) -> bytearray:
+        """Concrete implementation of abstract read method.
+
+        Since classes using abc.ABC can't be initialized if they have any abstract methods
+        which weren't overridden with a concrete implementation, this is a fake implementation,
+        without any actual logic, purely to allow the initialization of this class.
+
+        This method is expected to be mocked using ReadFunctionAsyncMock if it's expected to get called
+        during testing. If this method gets called without being mocked, it will raise NotImplementedError.
+        """
+        raise NotImplementedError(
+            "This concrete override of abstract read method isn't intended for actual use!\n"
+            " - If you're writing a new test, did you forget to mock it?\n"
+            " - If you're seeing this in an existing test, this method got called without the test expecting it,"
+            " this probably means you changed something in the code leading to this call, but you haven't updated"
+            " the tests to mock this function."
+        )
+
+
+class WrappedAsyncReader(SynchronizedMixin):
+    """Wrapped synchronous implementation of asynchronous AsyncReader class."""
+
+    _WRAPPED_ATTRIBUTE = "_reader"
+
+    def __init__(self):
+        self._reader = AsyncReader()
+
+
+class WrappedAsyncWriter(SynchronizedMixin):
+    """Wrapped synchronous implementation of asynchronous AsyncWriter class."""
+
+    _WRAPPED_ATTRIBUTE = "_writer"
+
+    def __init__(self):
+        self._writer = AsyncWriter()
+
+
+def to_two_complement(number: int, bytes: int) -> int:
+    """Helper function to convert a number into two's complement format."""
+    return number + 2 ** (bytes * 8)
+
+
+def from_two_complement(number: int, bytes: int) -> int:
+    """Helper function to get the real value from int in two's complement format."""
+    return number - 2 ** (bytes * 8) + 1
--- tests/protocol/test_abc.py
+++ tests/protocol/test_abc.py
@@ -1,190 +1,23 @@
 import inspect
 from abc import ABC, abstractmethod
-from typing import List, Optional, Union
+from typing import List, Union
 from unittest.mock import AsyncMock, Mock
 
 import pytest
 
 from mcproto.protocol.abc import BaseAsyncReader, BaseAsyncWriter, BaseSyncReader, BaseSyncWriter
-from tests.helpers import SynchronizedMixin
+from tests.protocol.helpers import (
+    ReadFunctionAsyncMock,
+    ReadFunctionMock,
+    SyncReader,
+    SyncWriter,
+    WrappedAsyncReader,
+    WrappedAsyncWriter,
+    WriteFunctionAsyncMock,
+    WriteFunctionMock,
+    to_two_complement,
+)
 
-# region: Helper classes/functions
-
-
-class WriteFunctionMock(Mock):
-    def __init__(self, *a, **kw):
-        super().__init__(*a, **kw)
-        self.combined_data = bytearray()
-
-    def __call__(self, data: bytearray) -> None:
-        """Override mock's __call__ to extend our combined_data bytearray.
-
-        This allows us to keep track of exactly what data was written by the mocked write function
-        in total, rather than only having tools like assert_called_with, which don't combine the
-        data of each call.
-        """
-        self.combined_data.extend(data)
-        return super().__call__(data)
-
-    def assert_has_data(self, data: bytearray, ensure_called: bool = True):
-        """Ensure that the total data to write by the mocked function matches expected data."""
-        if ensure_called:
-            self.assert_called()
-
-        if self.combined_data != data:
-            raise AssertionError(f"Write function mock expected data {data!r}, but was {self.call_data!r}")
-
-
-class WriteFunctionAsyncMock(WriteFunctionMock, AsyncMock):
-    ...
-
-
-class ReadFunctionMock(Mock):
-    def __init__(self, *a, combined_data: Optional[bytearray] = None, **kw):
-        super().__init__(*a, **kw)
-        if combined_data is None:
-            combined_data = bytearray()
-        self.combined_data = combined_data
-
-    def __call__(self, length: int) -> bytearray:
-        """Override mock's __call__ to make it return part of our combined_data bytearray.
-
-        This allows us to define the combined data we want the mocked read function to be
-        returning, and have each call only take requested part (length) of that data.
-        """
-        self.return_value = self.combined_data[:length]
-        del self.combined_data[:length]
-        return super().__call__(length)
-
-    def assert_read_everything(self):
-        """Ensure that the passed combined_data was fully read and depleted by one, or more calls."""
-        if len(self.combined_data) != 0:
-            raise AssertionError(
-                f"Read function didn't deplete all of it's data, remaining data: {self.combined_data!r}"
-            )
-
-
-class ReadFunctionAsyncMock(ReadFunctionMock, AsyncMock):
-    ...
-
-
-class SyncWriter(BaseSyncWriter):
-    """Initializable concrete implementation of BaseSyncWriter ABC."""
-
-    def write(self, data: bytearray) -> None:
-        """Concrete implementation of abstract write method.
-
-        Since classes using abc.ABC can't be initialized if they have any abstract methods
-        which weren't overridden with a concrete implementation, this is a fake implementation,
-        without any actual logic, purely to allow the initialization of this class.
-
-        This method is expected to be mocked using WriteFunctionMock if it's expected to get called
-        during testing. If this method gets called without being mocked, it will raise NotImplementedError.
-        """
-        raise NotImplementedError(
-            "This concrete override of abstract write method isn't intended for actual use!\n"
-            " - If you're writing a new test, did you forget to mock it?\n"
-            " - If you're seeing this in an existing test, this method got called without the test expecting it,"
-            " this probably means you changed something in the code leading to this call, but you haven't updated"
-            " the tests to mock this function."
-        )
-
-
-class SyncReader(BaseSyncReader):
-    """Testable concrete implementation of BaseSyncReader ABC."""
-
-    def read(self, length: int) -> bytearray:
-        """Concrete implementation of abstract read method.
-
-        Since classes using abc.ABC can't be initialized if they have any abstract methods
-        which weren't overridden with a concrete implementation, this is a fake implementation,
-        without any actual logic, purely to allow the initialization of this class.
-
-        This method is expected to be mocked using ReadFunctionMock if it's expected to get called
-        during testing. If this method gets called without being mocked, it will raise NotImplementedError.
-        """
-        raise NotImplementedError(
-            "This concrete override of abstract read method isn't intended for actual use!\n"
-            " - If you're writing a new test, did you forget to mock it?\n"
-            " - If you're seeing this in an existing test, this method got called without the test expecting it,"
-            " this probably means you changed something in the code leading to this call, but you haven't updated"
-            " the tests to mock this function."
-        )
-
-
-class AsyncWriter(BaseAsyncWriter):
-    """Initializable concrete implementation of BaseAsyncWriter ABC."""
-
-    async def write(self, data: bytearray) -> None:
-        """Concrete implementation of abstract write method.
-
-        Since classes using abc.ABC can't be initialized if they have any abstract methods
-        which weren't overridden with a concrete implementation, this is a fake implementation,
-        without any actual logic, purely to allow the initialization of this class.
-
-        This method is expected to be mocked using WriteFunctionAsyncMock if it's expected to get called
-        during testing. If this method gets called without being mocked, it will raise NotImplementedError.
-        """
-        raise NotImplementedError(
-            "This concrete override of abstract write method isn't intended for actual use!\n"
-            " - If you're writing a new test, did you forget to mock it?\n"
-            " - If you're seeing this in an existing test, this method got called without the test expecting it,"
-            " this probably means you changed something in the code leading to this call, but you haven't updated"
-            " the tests to mock this function."
-        )
-
-
-class AsyncReader(BaseAsyncReader):
-    """Testable concrete implementation of BaseAsyncReader ABC."""
-
-    async def read(self, length: int) -> bytearray:
-        """Concrete implementation of abstract read method.
-
-        Since classes using abc.ABC can't be initialized if they have any abstract methods
-        which weren't overridden with a concrete implementation, this is a fake implementation,
-        without any actual logic, purely to allow the initialization of this class.
-
-        This method is expected to be mocked using ReadFunctionAsyncMock if it's expected to get called
-        during testing. If this method gets called without being mocked, it will raise NotImplementedError.
-        """
-        raise NotImplementedError(
-            "This concrete override of abstract read method isn't intended for actual use!\n"
-            " - If you're writing a new test, did you forget to mock it?\n"
-            " - If you're seeing this in an existing test, this method got called without the test expecting it,"
-            " this probably means you changed something in the code leading to this call, but you haven't updated"
-            " the tests to mock this function."
-        )
-
-
-class WrappedAsyncReader(SynchronizedMixin):
-    """Wrapped synchronous implementation of asynchronous AsyncReader class."""
-
-    _WRAPPED_ATTRIBUTE = "_reader"
-
-    def __init__(self):
-        self._reader = AsyncReader()
-
-
-class WrappedAsyncWriter(SynchronizedMixin):
-    """Wrapped synchronous implementation of asynchronous AsyncWriter class."""
-
-    _WRAPPED_ATTRIBUTE = "_writer"
-
-    def __init__(self):
-        self._writer = AsyncWriter()
-
-
-def _to_two_complement(number: int, bytes: int) -> int:
-    """Helper function to convert a number into two's complement format."""
-    return number + 2 ** (bytes * 8)
-
-
-def _from_two_complement(number: int, bytes: int) -> int:
-    """Helper function to get the real value from int in two's complement format."""
-    return number - 2 ** (bytes * 8) + 1
-
-
-# endregion
 # region: Abstract test classes
 
 
@@ -246,7 +79,7 @@ class WriterTests(ABC):
     def test_write_byte_negative(self, write_mock: WriteFunctionMock):
         """Negative number bytes should be stored in two's complement format."""
         self.writer.write_byte(-20)
-        write_mock.assert_has_data(bytearray([_to_two_complement(-20, 1)]))
+        write_mock.assert_has_data(bytearray([to_two_complement(-20, 1)]))
 
     def test_write_byte_out_of_range(self):
         """Signed bytes should only allow writes from -128 to 127."""
@@ -320,8 +153,8 @@ class WriterTests(ABC):
             (0, 0),
             (120, 120),
             (2147483647, 2147483647),
-            (-1, _to_two_complement(-1, 4)),
-            (-2147483648, _to_two_complement(-2147483648, 4)),
+            (-1, to_two_complement(-1, 4)),
+            (-2147483648, to_two_complement(-2147483648, 4)),
         ),
     )
     def test_write_varint(self, varint_value: int, expected_varnum_call: int, autopatch):
@@ -438,8 +271,8 @@ class ReaderTests(ABC):
     @pytest.mark.parametrize(
         "read_bytes,expected_value",
         (
-            ([_to_two_complement(-20, 1)], -20),
-            ([_to_two_complement(-128, 1)], -128),
+            ([to_two_complement(-20, 1)], -20),
+            ([to_two_complement(-128, 1)], -128),
             ([20], 20),
             ([127], 127),
         ),
@@ -498,8 +331,8 @@ class ReaderTests(ABC):
             (0, 0),
             (120, 120),
             (2147483647, 2147483647),
-            (_to_two_complement(-1, 4), -1),
-            (_to_two_complement(-2147483648, 4), -2147483648),
+            (to_two_complement(-1, 4), -1),
+            (to_two_complement(-2147483648, 4), -2147483648),
         ),
     )
     def test_read_varint(self, varnum_return_value: int, expected_varint_value: int, autopatch):
