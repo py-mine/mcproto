a28eda055559f88fa2fa3bad7888fb4b509d1203
ItsDrike <itsdrike@protonmail.com>
2022-07-17 19:28:22 +0200

    Enforce ranges on twos complement conversions

--- mcproto/protocol/utils.py
+++ mcproto/protocol/utils.py
@@ -15,11 +15,22 @@ R = TypeVar("R")
 
 def to_twos_complement(num: int, bits: int) -> int:
     """Convert a given number into twos complement format of given amount of bits."""
+    value_max = 1 << (bits - 1)
+    value_min = value_max * -1
+    # With two's complement, we have one more negative number than positive
+    # this means we can't be exactly at value_max, but we can be at exactly value_min
+    if num >= value_max or num < value_min:
+        raise ValueError(f"Can't convert number {num} into {bits}-bit twos complement format - out of range")
+
     return num + (1 << bits) if num < 0 else num
 
 
 def from_twos_complement(num: int, bits: int) -> int:
     """Convert a given number from twos complement format of given amount of bits."""
+    value_max = (1 << bits) - 1
+    if num < 0 or num > value_max:
+        raise ValueError(f"Can't convert number {num} from {bits}-bit twos complement format - out of range")
+
     if num & (1 << (bits - 1)) != 0:
         num -= 1 << bits
 
