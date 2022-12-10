e89c7dedd85f9ac870a2be7502d28145d1506021
ItsDrike <itsdrike@protonmail.com>
2022-12-10 01:21:44 +0100

    Avoid resplitting elements in release script

--- release.sh
+++ release.sh
@@ -13,4 +13,4 @@ echo "Set these environment variables if you wish to not be pestered:"
 echo "export POETRY_PYPI_TOKEN_PYPI=my-token"
 echo "export POETRY_HTTP_BASIC_PYPI_USERNAME=username"
 echo "export POETRY_HTTP_BASIC_PYPI_PASSWORD=password"
-poetry publish $@
+poetry publish "$@"
