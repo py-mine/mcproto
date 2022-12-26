f13f9d95a4c3e08394d14b319b45f616dc90fea2
ItsDrike <itsdrike@protonmail.com>
2022-12-26 20:24:06 +0100

    Ensure deprecation warning/error contains object name

--- tests/utils/test_deprecation.py
+++ tests/utils/test_deprecation.py
@@ -8,7 +8,7 @@ from mcproto.utils.deprecation import deprecated, deprecation_warn
 
 def test_deprecation_warn_produces_error(monkeypatch: pytest.MonkeyPatch):
     monkeypatch.setattr(importlib.metadata, "version", lambda pkg: "1.0.0")
-    with pytest.raises(DeprecationWarning):
+    with pytest.raises(DeprecationWarning, match="test"):
         deprecation_warn(obj_name="test", removal_version="0.9.0")
 
 
@@ -17,6 +17,7 @@ def test_deprecation_warn_produces_warning(monkeypatch: pytest.MonkeyPatch):
     with warnings.catch_warnings(record=True) as w:
         deprecation_warn(obj_name="test", removal_version="1.0.1")
         assert issubclass(w[-1].category, DeprecationWarning)
+        assert "test" in str(w[-1].message)
 
 
 def test_deprecation_decorator(monkeypatch: pytest.MonkeyPatch):
@@ -29,3 +30,4 @@ def test_deprecation_decorator(monkeypatch: pytest.MonkeyPatch):
     with warnings.catch_warnings(record=True) as w:
         assert func(5) == 5
         assert issubclass(w[-1].category, DeprecationWarning)
+        assert "func" in str(w[-1].message)
