f6be8c12790619b925597863a5e92fa66a32da0a
Martin K <martyshkon@gmail.com>
2023-02-19 00:43:11 +0200

    Add tests for two's complement functions\` limits

--- tests/mcproto/protocol/test_utils.py
+++ tests/mcproto/protocol/test_utils.py
@@ -33,6 +33,24 @@ def test_to_twos_complement_negative(number: int, bits: int, expected_out: int):
 
 
 @pytest.mark.parametrize(
+    ("number", "bits"),
+    [
+        (128, 8),
+        (-129, 8),
+        (32768, 16),
+        (-32769, 16),
+        (2147483648, 32),
+        (-2147483649, 32),
+        (9223372036854775808, 64),
+        (-9223372036854775809, 64),
+    ],
+)
+def test_to_twos_complement_range(number: int, bits: int):
+    with pytest.raises(ValueError, match="out of range"):
+        to_twos_complement(number, bits)
+
+
+@pytest.mark.parametrize(
     ("number", "bits", "expected_out"),
     [
         (0, 8, 0),
@@ -55,3 +73,21 @@ def test_from_twos_complement_positive(number: int, bits: int, expected_out: int
 )
 def test_from_twos_complement_negative(number: int, bits: int, expected_out: int):
     assert from_twos_complement(number, bits) == expected_out
+
+
+@pytest.mark.parametrize(
+    ("number", "bits"),
+    [
+        (256, 8),
+        (-1, 8),
+        (65536, 16),
+        (-1, 16),
+        (4294967296, 32),
+        (-1, 32),
+        (18446744073709551616, 64),
+        (-1, 64),
+    ],
+)
+def test_from_twos_complement_range(number: int, bits: int):
+    with pytest.raises(ValueError, match="out of range"):
+        from_twos_complement(number, bits)
