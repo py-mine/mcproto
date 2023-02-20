f33333c9cd7b6d79a237fa4483ab845ad8f3bf3f
Martin K <martyshkon@gmail.com>
2023-02-21 01:30:33 +0200

    Test `PingPong` packet

---
+++ tests/mcproto/packets/v757/status/test_ping.py
@@ -0,0 +1,34 @@
+from __future__ import annotations
+
+from typing import Any
+
+import pytest
+
+from mcproto.buffer import Buffer
+from mcproto.packets.v757.status.ping import PingPong
+
+
+@pytest.mark.parametrize(
+    ("kwargs", "expected_bytes"),
+    [
+        ({"payload": 2806088}, bytes.fromhex("00000000002ad148")),
+        ({"payload": 123456}, bytes.fromhex("000000000001e240")),
+    ],
+)
+def test_serialize(kwargs: dict[str, Any], expected_bytes: list[int]):
+    ping = PingPong(**kwargs)
+    assert ping.serialize().flush() == bytearray(expected_bytes)
+
+
+@pytest.mark.parametrize(
+    ("read_bytes", "expected_out"),
+    [
+        (bytes.fromhex("00000000002ad148"), {"payload": 2806088}),
+        (bytes.fromhex("000000000001e240"), {"payload": 123456}),
+    ],
+)
+def test_deserialize(read_bytes: list[int], expected_out: dict[str, Any]):
+    ping = PingPong.deserialize(Buffer(read_bytes))
+
+    for i, v in expected_out.items():
+        assert getattr(ping, i) == v
