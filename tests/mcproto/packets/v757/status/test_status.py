a2a02dc69745a0cd7950c1fbe0119a6bb58f5a30
Martin K <martyshkon@gmail.com>
2023-02-22 18:24:59 +0200

    Use JSON library for `StatusResponse` test

--- tests/mcproto/packets/v757/status/test_status.py
+++ tests/mcproto/packets/v757/status/test_status.py
@@ -1,26 +1,63 @@
 from __future__ import annotations
 
+import json
 from typing import Any
 
 import pytest
 
+from mcproto.buffer import Buffer
 from mcproto.packets.v757.status.status import StatusResponse
 
 
 @pytest.mark.parametrize(
-    ("data"),
+    ("data", "expected_bytes"),
     [
         (
             {
                 "description": {"text": "A Minecraft Server"},
                 "players": {"max": 20, "online": 0},
                 "version": {"name": "1.18.1", "protocol": 757},
-            }
+            },
+            bytes.fromhex(
+                "797b226465736372697074696f6e223a7b2274657874223a2241204d696e6"
+                "5637261667420536572766572227d2c22706c6179657273223a7b226d6178"
+                "223a32302c226f6e6c696e65223a20307d2c2276657273696f6e223a7b226"
+                "e616d65223a22312e31382e31222c2270726f746f636f6c223a3735377d7d"
+            ),
         ),
     ],
 )
-def test_serialize_deserialize(data: dict[str, Any]):
-    # I had to combine the methods, because Minecraft returns a JSON with no spaces,
-    # but Python adds spaces after commas and colons.
-    status = StatusResponse(data=data)
-    assert StatusResponse.deserialize(status.serialize()).data == data
+def test_serialize(data: dict[str, Any], expected_bytes: bytes):
+    expected_buffer = Buffer(expected_bytes)
+    expected_buffer.read_varint()  # Clear the JSON length data.
+    expected_bytes = expected_buffer.flush()
+
+    buffer = StatusResponse(data=data).serialize()
+    buffer.read_varint()  # Clear the JSON length data.
+    out = buffer.flush()
+
+    assert json.loads(out) == json.loads(expected_bytes)
+
+
+@pytest.mark.parametrize(
+    ("read_bytes", "expected_data"),
+    [
+        (
+            bytes.fromhex(
+                "797b226465736372697074696f6e223a7b2274657874223a2241204d696e6"
+                "5637261667420536572766572227d2c22706c6179657273223a7b226d6178"
+                "223a32302c226f6e6c696e65223a20307d2c2276657273696f6e223a7b226"
+                "e616d65223a22312e31382e31222c2270726f746f636f6c223a3735377d7d"
+            ),
+            {
+                "description": {"text": "A Minecraft Server"},
+                "players": {"max": 20, "online": 0},
+                "version": {"name": "1.18.1", "protocol": 757},
+            },
+        ),
+    ],
+)
+def test_deserialize(read_bytes: list[int], expected_data: dict[str, Any]):
+    status = StatusResponse.deserialize(Buffer(read_bytes))
+
+    assert expected_data == status.data
